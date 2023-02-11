ee5f97eb2a060d0054e8bb89bd7f54ca974c1439
Nicholas Clarkin-Wright <nicholas.clarkin-wright@outlook.com>
2023-02-11 17:15:09 -0500

    Solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+function repeatString(string, num) {
+  
+  // Can't repeat negative numbers
+  if (num < 0) {
+    return 'ERROR';
+  }
+  
+  // Make copy of string to return
+  let repeatedString = '';
+  
+  for (let i = 0; i < num; i++) {
+    repeatedString = repeatedString.concat('', string);
+  }
 
+  return repeatedString;
 };
 
 // Do not edit below this line
