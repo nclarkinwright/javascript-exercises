b236b48af3a73fb872ae1f2a619b8c1bcc1d691b
Nicholas Clarkin-Wright <nicholas.clarkin-wright@outlook.com>
2023-02-12 15:38:23 -0500

    Solve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+function reverseString(string) {
 
+  let reversed = '';
+
+  // Loop through string backwards, and add each letter to reversed
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversed = reversed.concat('', string[i]);
+  }
+  
+  return reversed;
 };
 
 // Do not edit below this line
