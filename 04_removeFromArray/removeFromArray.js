7f74d632d180768bc6e6fc9e60c92a77a67efc8c
Nicholas Clarkin-Wright <nicholas.clarkin-wright@outlook.com>
2023-02-14 07:57:16 -0500

    Solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+function removeFromArray(array, ...toRemove) {
 
+  const filtered = array.slice();
+
+  // Remove each specified element
+  for (const element of toRemove) {
+    // Remove every occurrence of the element from the array
+    let index;
+    do {
+      index = filtered.indexOf(element);
+      if (index !== -1) {
+        filtered.splice(index, 1);
+      }
+    } while (index !== -1);
+    // Stop and move on to next element when current not found
+  }
+
+  console.log(filtered);
+
+  return filtered;
 };
 
 // Do not edit below this line
