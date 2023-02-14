d11c956ea1ea5697e99d65931af077bdc50d8866
Nicholas Clarkin-Wright <nicholas.clarkin-wright@outlook.com>
2023-02-14 09:24:42 -0500

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(n, m) {
 
+  if (n < 0 || m < 0 || typeof n !== 'number' || typeof m !== 'number') {
+    return 'ERROR';
+  }
+
+  let sum = 0;
+  const higher = n >= m ? n : m;
+  const lower = m <= n ? m : n;
+
+  for (let i = lower; i <= higher; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
