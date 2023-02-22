420eea53f03504e65edcaec91f309eda993d83cf
Nicholas Clarkin-Wright <nicholas.clarkin-wright@outlook.com>
2023-02-22 09:36:32 -0500

    Solve 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+  // Only accept valid member 
+  if (member <= 0) return 'OOPS';
+  // Return 1 if looking for first two members
+  if (member === 1 || member === 2) return 1;
+  // Calculate the nth element of the fibonacci sequence
+  let n = m = 1, next;
+  for (let i = 3; i <= member; i++) {
+    next = n + m;
+    m = n;
+    n = next;
+  }
 
+  return n;
 };
 
 // Do not edit below this line
