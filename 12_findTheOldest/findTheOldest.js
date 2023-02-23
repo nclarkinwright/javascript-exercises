234d0ec627a5ec8cdc5d9563ee886fc8081e2adc
Nicholas Clarkin-Wright <nicholas.clarkin-wright@outlook.com>
2023-02-23 08:16:48 -0500

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(persons) {
+  return persons.reduce((person, otherPerson) => {    
+    // Use death to calculate age, or present year if still alive
+    let now = new Date();
+    let currentYear = now.getFullYear();
+    let personEndDate = person.yearOfDeath ? person.yearOfDeath : currentYear;
+    let otherPersonEndDate = otherPerson.yearOfDeath ? otherPerson.yearOfDeath : currentYear;
+    
+    let personAge = personEndDate - person.yearOfBirth;
+    let otherPersonAge = otherPersonEndDate - otherPerson.yearOfBirth;
+    
+    return personAge >= otherPersonAge ? person : otherPerson;
+  });
 };
 
 // Do not edit below this line
